d54fb3d809f11ff0671b3a3e7538b2de45293146
Danny <dannywo@gmail.com>
2024-03-12 22:23:51 -0700

    creates new nav module

---
+++ components/NavBar.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function NavBar() {
+    const { user, username } = {};
+
+    return (
+        <nav className="navbar">
+
+        </nav>
+    )
+}
