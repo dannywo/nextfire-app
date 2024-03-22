3de10d305819749d4de0a487e6e60070a8109b35
Danny <dannywo@gmail.com>
2024-03-21 19:51:26 -0700

    Adds styling

--- app/enter/page.js
+++ app/enter/page.js
@@ -1,6 +1,6 @@
 export default async function Enter() {
     return (
-        <main>
+        <main className="flex min-h-screen flex-col items-center justify-center">
             <h1>Sign Up</h1>
         </main>
     )
