3c69c7603a544bb3f13293ce0f1ce5b9b489fc1e
Danny <dannywo@gmail.com>
2024-03-26 23:02:39 -0700

    Updates to layout

--- app/enter/page.js
+++ app/enter/page.js
@@ -1,7 +1,7 @@
 export default async function Enter() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-center">
+        <div className="flex min-h-screen flex-col items-center justify-center">
             <h1>Sign Up</h1>
-        </main>
+        </div>
     )
 }
